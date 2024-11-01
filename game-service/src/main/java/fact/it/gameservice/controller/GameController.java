4ca0dcebaa167b3600636442641a2e16edabb7b0
Bryan Antonis <bryan.antonis96@gmail.com>
2024-11-01 12:51:49 +0100

    simplified endpoints, made maven java image build on ARM64 too

--- game-service/src/main/java/fact/it/gameservice/controller/GameController.java
+++ game-service/src/main/java/fact/it/gameservice/controller/GameController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/games")
+@RequestMapping("/api/game")
 @RequiredArgsConstructor
 public class GameController {
 
