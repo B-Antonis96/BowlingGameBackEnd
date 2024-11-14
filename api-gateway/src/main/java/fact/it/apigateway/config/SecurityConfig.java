2d9096fc5d2a23d06d92d0c75b1eef03fcc48d8c
Bryan Antonis <bryan.antonis96@gmail.com>
2024-11-14 03:19:02 +0100

    OID half working

--- api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package fact.it.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -11,6 +12,7 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
@@ -18,23 +20,24 @@ public class SecurityConfig {
         @Bean
         public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
                 serverHttpSecurity
-                        .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                        .authorizeExchange(exchange -> exchange
-                                .pathMatchers("/api/**")
-                                .permitAll()
-                                .anyExchange()
-                                .authenticated()
+                        .authorizeExchange(exchange ->
+                                exchange.pathMatchers("/api/auth/login**", "/login**").permitAll()
+                                        .anyExchange().authenticated()
                         )
-                        .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+                        .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()))
+                        .oauth2Login(Customizer.withDefaults())
+                        .csrf(ServerHttpSecurity.CsrfSpec::disable);
                 return serverHttpSecurity.build();
         }
+
+
 
         @Bean
         public CorsWebFilter corsFilter() {
                 CorsConfiguration config = new CorsConfiguration();
                 config.setAllowCredentials(true);
-                config.addAllowedOrigin("http://antdom.orchestratix.com:61808/"); // Replace with your frontend origin
-                config.addAllowedHeader("*");
+                config.addAllowedOrigin("*");
+                config.addAllowedHeader("Authorization");
                 config.addAllowedMethod("*");
 
                 UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
